4038eaa1106c7ecf1c09ef0f34a41cb5b55c9cfc
AukeB <afbruinsma@gmail.com>
2025-12-07 15:38:48 +0200

    Updated

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from src.photo_merger.config_manager import ConfigManager
 from src.photo_merger.photo_merger import PhotoMerger
 
+from src.photo_merger.config_manager import ConfigManager
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
